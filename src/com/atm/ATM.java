416aff5a24ca6d99db629cefc7dac60eac4e7359
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2024-02-28 10:08:44 +0100

    Init Commit

--- src/com/atm/ATM.java
+++ src/com/atm/ATM.java
@@ -1,7 +1,34 @@
 package com.atm;
+import java.util.Scanner;
+
 
 public class ATM {
     public static void main(String[] args) {
+        BasicCard card = new BasicCard;
+        BasicCardReader cardReader = new BasicCardReaer;
+        BasicAccount account = new BasicAccount;
+        BasicSecuritySystem securitySystem = new BasicSecuritySystem;
+        BasicTransactionProcessor transactionProcessor = new BasicTransactionProcessor;
+        BasicCashDispenser cashDispenser = new BasicCashDispenser;
+
         System.out.println("Hello world!");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+        boolean quit = false;
+        if(!quit){
+            System.out.println("not quit");
+
+            if(!input.equals("quit")){
+                cardReader.validate(card);
+                String pin = scanner.next();
+
+            } else {
+                quit = true;
+
+            }
+        }
     }
 }
