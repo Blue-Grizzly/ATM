ec4963a90097bf9a9be18a4d8273dc9ebe37fe2b
Magnus Carstensen <brondby_1964@hotmail.com>
2024-02-28 11:10:23 +0100

    transaction hopefully done

--- src/com/atm/transaction/BasicTransactionProcessor.java
+++ src/com/atm/transaction/BasicTransactionProcessor.java
@@ -1,4 +1,18 @@
 package com.atm.transaction;
 
-public class BasicTransactionProcessor {
-}
+public class BasicTransactionProcessor implements TransactionProcessor {
+
+    public void processTransaction(Account account, double amount){
+        if (account != null){
+            if(account.withdraw(amount)){
+                System.out.println("Transaction processed for amount: $" + amount);
+            } else {
+                System.out.println("Transaction denied");
+            }}
+        else {
+                System.out.println("Account doesn't exist");
+            }
+
+        }
+    }
+
--- src/com/atm/transaction/TransactionProcessor.java
+++ src/com/atm/transaction/TransactionProcessor.java
@@ -1,6 +1,7 @@
 package com.atm.transaction;
 
 public interface TransactionProcessor {
-    
+    public void processTransaction(Account account, double amount);
+
 
 }
