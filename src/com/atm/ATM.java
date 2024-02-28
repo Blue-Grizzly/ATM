95917e83e479bbc240f4f09e265d8b67bb2a3d8d
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2024-02-28 11:19:05 +0100

    Update ATM.java

--- src/com/atm/ATM.java
+++ src/com/atm/ATM.java
@@ -1,35 +1,54 @@
 package com.atm;
+import com.atm.account.Account;
+import com.atm.account.BasicAccount;
 import com.atm.card.BasicCard;
+import com.atm.card.BasicCardReader;
+import com.atm.card.Card;
 
 import java.util.Scanner;
-
 
 public class ATM {
     public static void main(String[] args) {
         BasicCard card = new BasicCard;
-        BasicCardReader cardReader = new BasicCardReaer;
+        BasicCardReader cardReader = new BasicCardReader;
         BasicAccount account = new BasicAccount;
         BasicSecuritySystem securitySystem = new BasicSecuritySystem;
         BasicTransactionProcessor transactionProcessor = new BasicTransactionProcessor;
         BasicCashDispenser cashDispenser = new BasicCashDispenser;
 
-        System.out.println("This is an ATM system!");
-
-
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.next();
         boolean quit = false;
-        if(!quit){
-            System.out.println("not quit");
 
-            if(!input.equals("quit")){
-                cardReader.validate(card);
-                String pin = scanner.next();
+        while(!quit){
+            System.out.println("+------------------------+");
+            System.out.println("|                        |");
+            System.out.println("|-This is an ATM system!-|");
+            System.out.println("|                        |");
+            System.out.println("+------------------------+");
+
+            if(input.equals("quit")){
+                Card card = cardReader.validateCard(scanner.next());
+                if(card != null){
+                    Account account = card.getAccount();
+
+                    if(securitySystem.authenticate(card.getPin(), pinCode())){
+                        System.out.println(MSG_INPUT_AMOUNT);
+                        double amount = Double.parseDouble(scanner.next());
+
+                        transactionProcessor.processTransaction(account,amount);
+                        cashDispenser.dispenseCash(amount);
+                    } else {
+                        System.out.println(ERR_AUTHENTICATION);
+                    }
+                } else {
+                    System.out.println(ERR_INVALID_CARD);
+                }
 
             } else {
                 quit = true;
-
+                System.out.println(MSG_QUIT_RECIEVED);
             }
         }
     }
